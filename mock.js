f1ebcc0ba1e3cfd495cd74554dbc5432076144c9
Joe Zhu <820839844@qq.com>
2018-01-16 00:13:22 +0800

    Add files via upload
    deploy

--- mock.js
+++ mock.js
@@ -4,225 +4,158 @@ let http  = require('http');
 let querystring = require('querystring');
 let cheerio = require('cheerio');
 let superagent = require('superagent');
+let schedule = require('node-schedule');
 
-//the key params
-let JSESSIONID;
-let location;
-let CODE;
-let token;
-let testPath;
-let tokenPth;
-let _csrf;
-let page;
-let contents1;
-let contents2;
+const users = [
+    {
+        userName:'zjq@sccas.cn',
+        password:'1993608acz'
+    },
+    {
+        userName:'liyan@sccas.cn',
+        password:'sccas_921220'
+    }
+];
+const signInHead = querystring.stringify({
+    'response_type': 'code',
+    'redirect_uri': 'http://159.226.29.10/CnicCheck/testtoken',
+    'client_id': '58861',
+    'theme': 'simple',
+    'pageinfo': 'userinfo',
+    'tm': 0
+});
+
+function initUser(user){
+    return querystring.stringify(user)+'&'+signInHead;
+}
 
 //抓取页面_csrf字段
-function getCSRF(){
-    superagent.get('https://passport.escience.cn').end(function(err, res){
-        let $ = cheerio.load(res.text);
-        let inputs = $('input');
-        _csrf = inputs[6].attribs.value;
-        console.log("获得_csrf:"+_csrf);
-        initUser();
-    });
-}
-function initUser(){
-    contents1 = querystring.stringify({
-        'returnUrl': ' ' , 
-        'act': 'Validate',
-        'username': 'zjq@sccas.cn',
-        'password': '1993608acz',
-        '_csrf': _csrf
-    });
-    contents2 = {
-        returnUrl: ' ' , 
-        act: 'Validate',
-        username: 'zjq@sccas.cn',
-        password: '1993608acz',
-        _csrf: _csrf
-    };
-    console.log(_csrf);
-}
+// function getCSRF(){
+//     superagent.get('https://passport.escience.cn').end(function(err, res){
+//         let $ = cheerio.load(res.text);
+//         let inputs = $('input');
+//         _csrf = inputs[6].attribs.value;
+//         console.log("获得_csrf:"+_csrf);
+//         initUser();
+//     });
+// }
+// function initUser(){
+//     user = querystring.stringify({
+//         'returnUrl': ' ' , 
+//         'act': 'Validate',
+//         'username': 'zjq@sccas.cn',
+//         'password': '1993608acz',
+//         '_csrf': _csrf
+//     });
+// }
 
-getCSRF();
-
-let optIndex = {   //pre-signIn request header
-    hostname : 'passport.escience.cn',
-    path : '/index.jsp',
-    method: 'post',
-}
-let optAuth = {//auth request header
-    hostname : '159.226.29.10',
-    path: '/CnicCheck/authorization',
-    method: 'get',
-    headers: {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9',
-        'Connection': 'keep-alive',
-        'Host': '159.226.29.10',
-        'Upgrade-Insecure-Requests': 1,
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3315.4 Safari/537.36'
+//sign in
+let optLogin = {  //signIn request header
+    'hostname' : 'passport.escience.cn',
+    'path' : '/oauth2/authorize',
+    'method': 'post',
+    'headers': {
+        // 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        // 'Accept-Encoding': 'gzip, deflate, br',
+        // 'Accept-Language': 'zh-CN,zh;q=0.9',
+        // 'Cache-Control': 'max-age=0',
+        // 'Connection': 'keep-alive',
+        // 'Content-Length': user.length,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        // 'Cookie': 'AUTO_FILL="zjq@sccas.cn; '+JSESSIONID,
+        // 'Host': 'passport.escience.cn',
+        // 'Origin': 'https://passport.escience.cn',
+        // 'Referer': 'https://passport.escience.cn/login',
+        // 'Upgrade-Insecure-Requests': 1,
+        // 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3315.4 Safari/537.36'
     }
-};
-let optLogin = {};
-//to index.jsp get JESESSION
-let tick_Index = setTimeout(function(){
-    let reqIndex =  https.request(optIndex,res=>{
-        console.log(contents1);
+}
+//mock in and out
+function mock(user,checktype){
+    //the key params
+    let location;
+    let CODE;
+    let token;
+    let reqLogin = https.request(optLogin,function(res){
         console.log(res.statusCode);
         res.setEncoding('utf8');
         let headers = res.headers;
-        let cookies = headers['set-cookie'];
-        JSESSIONID = cookies[0].split(';')[0];//get the session ID
-        console.log('获取会话session:',JSESSIONID);
-        optLogin = {  //signIn request header
-            hostname : 'passport.escience.cn',
-            path : '/login',
-            method: 'post',
-            headers: {
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                'Accept-Encoding': 'gzip, deflate, br',
-                'Accept-Language': 'zh-CN,zh;q=0.9',
-                'Cache-Control': 'max-age=0',
-                'Connection': 'keep-alive',
-                'Content-Length': contents2.length,
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Cookie': 'AUTO_FILL="zjq@sccas.cn; '+JSESSIONID,
-                'Host': 'passport.escience.cn',
-                'Origin': 'https://passport.escience.cn',
-                'Referer': 'https://passport.escience.cn/login',
-                'Upgrade-Insecure-Requests': 1,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3315.4 Safari/537.36'
+        console.log('登录：');
+        //let cookies = headers['set-cookie'];
+        location = headers['location'];
+        console.log('location:',location);
+    });
+    reqLogin.write(user);
+    reqLogin.end();
+    
+    //get token
+    let tick = setTimeout(function(){
+        superagent.get(location).end(function(err, res){
+            console.log(res.text); 
+            let temp =JSON.parse(res.text);  
+            token = temp['token'];
+            console.log(token);     
+        });
+    },2000); 
+    
+    let mainUrl = '159.226.29.10',
+        posUrl = '/CnicCheck/CheckServlet?weidu=39.97943495591847&jingdu=116.32850166009715';
+    //check-in-out
+    let tock = setTimeout(function(){
+        let checkUrl =  mainUrl + posUrl + '&type='+ checktype +'&token='+token;
+        superagent.get(checkUrl).end(function(err,res){
+            console.log(res.text);
+            let temp = JSON.parse(res.text);
+            if(temp.success == 'true'){
+                if(checktype == 'checkout'){
+                    console.log('打卡收工！');
+                }else{
+                    console.log('打卡上班！');
+                }
+            }
+        });
+    },9000);
+}
+//entrance
+function main(){
+    let today = new Date(),
+        year = today.getFullYear(),
+        month = today.getMonth()+1,
+        day = today.getDate(),
+        hour = today.getHours();
+    let checkType = 'checkin';
+    if(hour>=18 && hour<24){
+        checkType = 'checkout';
+    }
+    date = `${year}${month}${day}`;
+    console.log(date);
+    const holidayAPI = 'http://api.goseek.cn/Tools/holiday?date=';
+    let isHoliday = holidayAPI + date;
+    superagent.get(isHoliday).end(function(err,res){
+        if(err){//失败则强行打卡
+            users.forEach(function(user){
+                let userForm = initUser(user);
+                mock(userForm,checkType);
+            });
+        }
+        else{
+            let temp = JSON.parse(res.text);
+            console.log(temp.data);
+            if(temp.data == 0){//0工作日打卡，1,2节假日不打卡
+                users.forEach(function(user){
+                    let userForm = initUser(user);
+                    mock(userForm,checkType);
+                })
             }
         }
     });
-    reqIndex.write(contents1);
-    reqIndex.end();
-},1000);
+}
+let rule = new schedule.RecurrenceRule();
+let times = [8,19,23];
+    rule.hour = times;
+    rule.minute = 1;
 
-//sign in
-let tick_SignIn = setTimeout(function(){
-    console.log('会话ID:'+JSESSIONID);
-        //  let optLogin = {  //signIn request header
-        //     hostname : 'passport.escience.cn',
-        //     path : '/login',
-        //     method: 'post',
-        //     headers: {
-        //         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        //         'Accept-Encoding': 'gzip, deflate, br',
-        //         'Accept-Language': 'zh-CN,zh;q=0.9',
-        //         'Cache-Control': 'max-age=0',
-        //         'Connection': 'keep-alive',
-        //         'Content-Length': contents2.length,
-        //         'Content-Type': 'application/x-www-form-urlencoded',
-        //         'Cookie': 'AUTO_FILL="zjq@sccas.cn; '+JSESSIONID,
-        //         'Host': 'passport.escience.cn',
-        //         'Origin': 'https://passport.escience.cn',
-        //         'Referer': 'https://passport.escience.cn/login',
-        //         'Upgrade-Insecure-Requests': 1,
-        //         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3315.4 Safari/537.36'
-        //     }
-        // }
-        // let reqLogin = https.request(optLogin,function(res){
-        //     console.log(res.statusCode);
-        //     res.setEncoding('utf8');
-        //     let headers = res.headers;
-        //     console.log('登录：');
-        //     let cookies = headers['set-cookie'];
-        //     JSESSIONID = cookies[0].split(';')[0];//get the session ID
-        //     console.log(JSESSIONID);
-        // });
-        // reqLogin.write(contents1);
-        // reqLogin.end();
-        superagent.post('https://passport.escience.cn/login').set(optLogin).send(contents2).redirects(0).end(function(err,res){
-            if(err){
-                console.log(err);
-            }else{
-                cookie = res.headers['set-cookie'];
-                console.log(res.headers);
-            }
-        });
-},4000);
+schedule.scheduleJob(rule,function(){
+    main();
+});
 
-
-//authorization
-let tock = setTimeout(function(){
-    
-  let reqAuth = http.request(optAuth,function(res){
-      console.log(res.statusCode);
-      res.setEncoding('utf8');
-      let headers = res.headers;
-      location = headers.location;
-      console.log('认证路径：');
-      console.log(location);
-      testPath = location.split('cn')[1];
-      console.log(testPath);
-    });
-   reqAuth.end();
-},5000);
-
-
-//return CODE
-// let tick = setTimeout(function(){
-//     let optTest = {
-//         hostname: 'passport.escience.cn',
-//         path: testPath ,
-//         method: 'get',
-//         headers: {
-//            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-//            'Accept-Encoding': 'gzip, deflate, br',
-//            'Accept-Language': 'zh-CN,zh;q=0.9',
-//            'Connection': 'keep-alive',
-//            'Cookie': `AUTO_FILL="zjq@sccas.cn" ;${JSESSIONID}`,
-//            'Host': 'passport.escience.cn',
-//            'Upgrade-Insecure-Requests': 1,
-//            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3315.4 Safari/537.36'
-//         }
-//     };
-//     let reqTest = https.request(optTest,function(res){
-//         console.log(res.statusCode);
-//         res.setEncoding('utf8');
-//         let headers = res.headers;
-//         console.log('获取code:');
-//         console.log(headers);
-//         //CODE = headers.location.split('?')[1].split('=')[1];//get the CODE
-//         console.log('code:'+CODE);
-//     });
-//     reqTest.end();   
-// },4000);
-
-// let optToken = {
-//     hostname: '159.226.29.10',
-//     path: '/CnicCheck/testtoken?code=' + CODE,
-//     method: 'get',
-//     headers: {
-//         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-//         'Accept-Encoding': 'gzip, deflate',
-//         'Accept-Language': 'zh-CN,zh;q=0.9',
-//         'Connection': 'keep-alive',
-//         'Host': '159.226.29.10',
-//         'Upgrade-Insecure-Requests': 1,
-//         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3315.4 Safari/537.36'
-//     }
-// }
-// //return token
-// // let reqToken = http.request(optToken,function(res){
-// //     console.log(res.statusCode);
-// //     res.setEncoding('utf8');
-// //     let headers = res.headers;
-// //     console.log(headers);
-// // });
-
-// // let optOut = {
-// //     hostname: '159.226.29.10 ',
-// //     path: '/CnicCheck/CheckServlet?weidu=39.97943495591847&jingdu=116.32850166009715&type=checkout&token=' + TOKEN,
-// //     methods: 'get',
-// //     headers: {
-
-// //     }
-// // }
-// // let reqOut = http.request(optOut,(res)=>{
-// //     console.log(res.header);
-// // });
